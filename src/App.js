ab4cfcf20bb6aec54d1a22814842d8de7ad68872
jshubham895 <54178334+jshubham895@users.noreply.github.com>
2023-09-23 15:36:40 +0530

    stackblitz changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import "./styles.css";
-import AppRoutes from "./routes/AppRoutes";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react';
+import './styles.css';
+import AppRoutes from './routes/AppRoutes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-	return (
-		<div className="App">
-			<ToastContainer autoClose={3000} />
-			<AppRoutes />
-		</div>
-	);
+  return (
+    <div className="App">
+      <ToastContainer autoClose={3000} />
+      <AppRoutes />
+    </div>
+  );
 }
 
 export default App;
